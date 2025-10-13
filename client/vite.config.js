ed9d96cba910252994c2ff3e1cabb0489eb5a7da
VISHAL KUMAR <vishalkumjun589@gmail.com>
2025-10-13 18:45:11 +0530

    Fixed CSS build path for Vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ✅ Very important for Vercel builds
 export default defineConfig({
   plugins: [react()],
+  base: './', 
   define: {
     'process.env': {}
   }
